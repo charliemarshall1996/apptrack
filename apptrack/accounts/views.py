5fa07f4dacf29a0711a2cf4549d0f15a4787a912
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-09-26 11:43:38 +0100

    created setup.py, MANIFEST.in

---
+++ apptrack/accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
