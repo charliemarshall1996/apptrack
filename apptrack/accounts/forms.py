cfba3cfd4f1c9c7e16e2e1f8a9892128cd55ba96
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-10-08 13:41:27 +0100

    fixed registration form

--- apptrack/accounts/forms.py
+++ apptrack/accounts/forms.py
@@ -1,8 +1,10 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 from .models import Profile
+
+User = get_user_model()
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -10,8 +12,19 @@ class UserRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1',
-                  'password2']
+        fields = ['email', 'password1',
+                  'password2', 'first_name', 'last_name']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].required = True
+        self.fields["password1"].required = True
+        self.fields["password2"].required = True
+        self.fields["first_name"].required = True
+        self.fields["last_name"].required = True
+
+    def save(self):
+        return super().save(commit=False)
 
 
 class ProfileRegistrationForm(forms.ModelForm):
@@ -20,17 +33,17 @@ class ProfileRegistrationForm(forms.ModelForm):
         fields = ['birth_date', 'gender']
 
         widgets = {
-            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+            'birth_date': forms.DateInput(attrs={'type': 'date'}),
         }
 
-    def save(self):
+    def save(self) -> Profile:
         return super().save(commit=False)
 
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['email']
 
 
 class ProfileUpdateForm(forms.ModelForm):
