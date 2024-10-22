c535121d4dbfcfb097348e626ea7b324d79664fc
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-10-22 11:25:06 +0100

    added test for logged in JobsListView response

--- apptrack/jobs/tests.py
+++ apptrack/jobs/tests.py
@@ -1,11 +1,23 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
 from django.urls import reverse
-from django.test import Client
+
 from .views import JobsListView
+
+User = get_user_model()
 
 
 class TestJobsListView(TestCase):
-    def test_get_jobs_list_view(self):
+
+    def test_view_requires_login(self):
         client = Client()
         response = client.get(reverse('jobs:list'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_logged_in_view(self):
+        user = User.objects.create_user(
+            email='testuser@test.com', password='password')
+        self.client.login(email='testuser@test.com', password='password')
+        url = reverse('jobs:list')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
