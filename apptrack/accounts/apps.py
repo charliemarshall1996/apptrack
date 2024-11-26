28e3da8be59ef686ee6358929b8fd7003a43e06c
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-11-26 13:15:28 +0000

    tested apps.py in accounts

--- apptrack/accounts/apps.py
+++ apptrack/accounts/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
 
+
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
-    # add this
     def ready(self):
         import accounts.signals  # noqa
