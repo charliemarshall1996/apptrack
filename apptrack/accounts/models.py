75595816d7e9217433a2b24a66511b2b0a54a92d
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-11-26 13:06:59 +0000

    removed Locations import into accounts.models.py

--- apptrack/accounts/models.py
+++ apptrack/accounts/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Locations
 from .managers import CustomUserManager
 # Create your models here.
 
