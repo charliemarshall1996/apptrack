b91f141f74c391d8cae509859e40a99ff53c38a8
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-11-28 09:52:15 +0000

    removed one of two JobsConfig from jobs.apps.py

--- apptrack/jobs/apps.py
+++ apptrack/jobs/apps.py
@@ -5,9 +5,5 @@ class JobsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'jobs'
 
-
-class JobsConfig(AppConfig):
-    name = 'jobs'
-
     def ready(self):
         import jobs.signals  # Import the signals to connect them
