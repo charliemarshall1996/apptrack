b67ea8edf2b76dac010e1c97ba4f66490c548146
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-11-28 15:00:37 +0000

    added tests for JobDeleteView

--- apptrack/jobs/tests/test_views.py
+++ apptrack/jobs/tests/test_views.py
@@ -97,3 +97,33 @@ def test_assign_job_view(client, board_factory, profile_factory, job_factory):
         assert job.column.id == columns[1]
     except AssertionError:
         print(f"JOB COLUMN {job.column.id}\nI1 COLUMN: {columns[1]}")
+
+
+@pytest.mark.django_db
+def test_delete_job_view(client, job_factory, profile_factory, board_factory):
+    PASSWORD = "securepassword"
+
+    profile = profile_factory(password=PASSWORD)
+    profile.save()
+
+    board = board_factory(user=profile.user)
+    board.save()
+
+    job = job_factory(user=profile.user)
+    job.board = board
+    job.save()
+
+    response = client.post(reverse("accounts:login"), {
+        "email": profile.user.email, "password": PASSWORD})
+
+    assert response.status_code == 302
+
+    url = reverse("jobs:delete_job", kwargs={"pk": job.pk})
+    response = client.get(url)
+
+    try:
+        assert response.status_code == 200
+    except AssertionError:
+        print(response.url)
+
+    assert job.DoesNotExist
