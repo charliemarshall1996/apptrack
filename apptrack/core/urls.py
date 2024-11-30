182555b9bb774293bfbc8aa3322372cb3264e47c
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-11-30 15:18:02 +0000

    removed get_sub_divisions from core.urls.py

--- apptrack/core/urls.py
+++ apptrack/core/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
     path("", views.home_view, name="home"),
     path("login/", auth_views.LoginView.as_view(template_name="core/login.html"), name="login"),
     path('contact/', views.ContactView.as_view(template_name='core/contact.html'), name='contact'),
-    path('get-subdivisions/<str:country_code>/',
-         views.get_subdivisions, name='get_subdivisions'),
     path('privacy-policy/', views.privacy_policy_view, name='privacy_policy'),
 ]
