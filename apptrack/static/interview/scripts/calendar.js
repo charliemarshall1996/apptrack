7b4368937da4599139caea66aab3c056d0c8c120
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-12-01 17:28:02 +0000

    changes to build-and-test.yml

--- apptrack/static/interview/scripts/calendar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var events = JSON.parse('{{ interviews|escapejs }}');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      events: events
-    });
-    calendar.render();
-  });
