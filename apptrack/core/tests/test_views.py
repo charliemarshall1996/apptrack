93e9fc58da7b0b15e39fddeab2ea545729cb11bd
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-12-06 16:04:48 +0000

    added test for privacy_policy_view

--- apptrack/core/tests/test_views.py
+++ apptrack/core/tests/test_views.py
@@ -8,3 +8,9 @@ def test_home_view(client):
     url = reverse('home')
     response = client.get(url)
     assert response.status_code == 200
+
+
+def test_privacy_policy_view(client):
+    url = reverse('privacy_policy')
+    response = client.get(url)
+    assert response.status_code == 200
