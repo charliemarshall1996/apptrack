9e2c982a7ae78be73145472ccbae8d919708a4a1
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-12-07 14:25:55 +0000

    added assertion that str(BlogPost) is same as title property

--- apptrack/blog/tests/test_models.py
+++ apptrack/blog/tests/test_models.py
@@ -12,3 +12,4 @@ def test_blog_post(blog_post_data_factory):
     assert post.content == data['content']
     assert post.summary == data['summary']
     assert post.published == data["published"]
+    assert str(post) == data["title"]
