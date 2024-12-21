aa4be31f0dbd5b5ce92dd5ed8affb50039009ba4
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-12-21 13:57:59 +0000

    added docstrings for job_edit.py

--- apptrack/jobs/views/job_edit.py
+++ apptrack/jobs/views/job_edit.py
@@ -1,3 +1,7 @@
+"""Job edit view.
+
+Manages the job edit view for the requesting user.
+"""
 import logging
 
 from django.contrib.auth.decorators import login_required
@@ -13,6 +17,18 @@ logger = logging.getLogger(__name__)
 @login_required
 @require_POST
 def job_edit_view(request, pk):
+    """Manages POST requests for the job edit view. GET requests are not supported.
+
+    This view handles the editing of a job for the requesting user. If the request
+    method is POST, it validates the form and saves the changes.
+
+    Args:
+        request (HttpRequest): The request object.
+        pk (int): The primary key of the job to edit.
+
+    Returns:
+        HttpResponseRedirect: The response object.
+    """
     logger.info("Editing job...")
     job = Job.objects.get(pk=pk)
     referer_url = request.POST.get("editJobReferrer")
