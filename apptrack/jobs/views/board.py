1d8ad12b206f832d69ca54960474b3c3401ca7c6
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-12-21 14:38:25 +0000

    added docstrings for board.py

--- apptrack/jobs/views/board.py
+++ apptrack/jobs/views/board.py
@@ -1,3 +1,4 @@
+"""Allows for a kanban-based view for the requesting user's jobs."""
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render
@@ -11,12 +12,27 @@ from jobs.forms import JobForm
 @require_GET
 def board_view(request):
     # Retrieve context data
-    jobs = Job.objects.filter(profile=request.user.profile, archived=False).all()
+    """Retrieves context data for the requesting user's job board view.
+
+    This view renders the jobs kanban template with the following context data:
+        - The requesting user's ID
+        - The requesting user's job board
+        - The columns associated with the job board
+        - The requesting user's jobs
+        - A blank job form
+        - A dictionary of job forms for each job, keyed by job ID
+
+    If the job board does not exist for the requesting user, it creates a new
+    job board and sends a warning message.
+    """
+    jobs = Job.objects.filter(
+        profile=request.user.profile, archived=False).all()
     board, created = Board.objects.get_or_create(profile=request.user.profile)
     if created:
         messages.warning(
             request,
-            "Job board was not found for your profile. A new job board has been created.",
+            "Job board was not found for your profile.\
+                  A new job board has been created.",
         )
     columns = board.columns.all()
     job_form = JobForm()
