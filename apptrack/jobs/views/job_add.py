f771b4cd6789af7441eeaa5acf2ea2bcf9cff061
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-12-21 14:24:33 +0000

    ammended job_add_view to always redirect to the referring url with a response message depending on the form validity.

--- apptrack/jobs/views/job_add.py
+++ apptrack/jobs/views/job_add.py
@@ -1,6 +1,7 @@
 """Manages the adding of jobs for the requesting user."""
 import logging
 
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.views.decorators.http import require_POST
@@ -25,22 +26,24 @@ def job_add_view(request):
         request (HttpRequest): The request object containing form data.
 
     Returns:
-        HttpResponseRedirect: Redirects to the referring URL if the form is valid.
+        HttpResponseRedirect: Redirects to the referring URL. If the form is valid, the 
+            response will contain a success message. If the form is invalid, the 
+                response will contain an error message.
     """
-    logger.debug("Adding job...")
     board = Board.objects.filter(profile=request.user.profile).first()
     referer_url = request.POST.get("referrer")
-    logger.debug("Referer URL: %s", referer_url)
+
     if request.method == "POST":
-        logger.debug("Request method is POST")
         form = JobForm(request.POST)
+
         if form.is_valid():
-            logger.debug("Form is valid")
             job = form.save()
             job.board = board
             job.profile = request.user.profile
             job.save()
-            logger.debug("Job saved")
-            return redirect(referer_url)
+            messages.success(request, "Job added successfully!")
         else:
-            logger.debug("Form is not valid %s", form.errors)
+            messages.error(request, f"Failed to add job {form.errors}. \
+                           Please try again.")
+
+    return redirect(referer_url)
