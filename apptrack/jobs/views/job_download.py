b7d1ce62a40f3d9709989ddba5cfecd13612fe21
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-12-21 14:01:54 +0000

    added docstrings for job_download.py

--- apptrack/jobs/views/job_download.py
+++ apptrack/jobs/views/job_download.py
@@ -1,3 +1,9 @@
+"""This module processes the job download view.
+
+Manages the job download view for the requesting user. Allowing them to download a CSV 
+file of their jobs based on a provided date range.
+"""
+
 import csv
 
 from django.contrib.auth.decorators import login_required
@@ -10,7 +16,24 @@ from jobs.models import Job
 
 @login_required
 def job_download_view(request):
-    # Handle GET request (date input form rendering)
+    """Manages the job download view for the requesting user.
+
+    Allows the user to download a CSV file of their jobs based on a provided date range.
+    If the request method is GET, it renders a form for the user to input the date range.
+    If the request method is POST, it generates a CSV file with the jobs in the specified
+    date range and returns it as a response.
+
+    The CSV file will have the following columns: ID, Job Title, Company, URL, Status,
+    and Updated.
+
+    Args:
+        request: The current request
+
+    Returns:
+        HttpResponse: A CSV file with the jobs in the specified date range if the 
+            request method is POST, or a form for the user to input the date range if the 
+                request method is GET
+    """
     if request.method == "GET":
         form = DownloadJobsForm()
         return render(
@@ -24,7 +47,8 @@ def job_download_view(request):
     end_date = request.POST.get("end_date")
 
     # Filter jobs based on the `updated` field
-    jobs = Job.objects.filter(updated__range=(start_date, end_date)).order_by("id")
+    jobs = Job.objects.filter(updated__range=(
+        start_date, end_date)).order_by("id")
 
     filename = f"jobs_{start_date}_{end_date}.csv"
     # Prepare the response
