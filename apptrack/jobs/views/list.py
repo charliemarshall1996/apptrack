0ac6b61e97e1ecbaac9047b80c166664c5738849
Charlie Marshall <charlie.marshall1996@gmail.com>
2025-01-29 09:09:37 +0000

    added sorting to list headers

--- apptrack/jobs/views/list.py
+++ apptrack/jobs/views/list.py
@@ -11,62 +11,19 @@ from jobs.forms import JobForm, JobFilterForm
 
 
 class JobListView(LoginRequiredMixin, ListView):
-    """Manages the job list view.
-
-    Inherits from the ListView class and adds a form to the context.
-    It provides functionality to paginate and filter the job list for the requesting 
-    user.
-
-    Attributes:
-        model (class): The model class for the view.
-        template_name (str): The name of the template to render.
-        context_object_name (str): The name of the object in the context.
-        paginate_by (int): The number of items to display per page.
-        ordering (str): The field to order the queryset by.
-    """
-
     model = Job
     template_name = "jobs/list.html"
     context_object_name = "jobs"
     paginate_by = 10
     ordering = ["-updated"]
 
-    def _archive_filter(self, queryset, filter_val):
-        """Archive filter helper.
-
-        Takes a queryset and a filter value and filters the queryset according to the
-        filter value.
-
-        Args:
-            queryset (QuerySet): The queryset to filter.
-            filter_val (str): The filter value. Must be one of "in", "on", or "ex".
-
-        Returns:
-            QuerySet: The filtered queryset.
-        """
-        if filter_val != "in":
-            # If the archived filter is set to "only",
-            # filter the queryset to show only archived jobs
-            if filter_val == "on":
-                queryset = queryset.filter(archived=True)
-
-            # If the archived filter is set to "exclude",
-            # filter the queryset to exclude archived jobs
-            else:
-                queryset = queryset.exclude(archived=True)
-
-        return queryset
+    def get_ordering(self):
+        """Get the ordering for the queryset based on the request parameters."""
+        ordering = self.request.GET.get('ordering', '-updated')
+        return ordering
 
     def get_queryset(self):
-        """Get the filtered queryset.
-
-        Gets the queryset from the parent class and filters it.
-        It always filters by the user's profile. After that, it uses fields and values
-        from the JobFilterForm to filter the queryset.
-
-        Returns:
-            QuerySet: The filtered queryset.
-        """
+        """Get the filtered and sorted queryset."""
         queryset = super().get_queryset()
         queryset = queryset.filter(profile=self.request.user.profile)
 
@@ -83,8 +40,10 @@ class JobListView(LoginRequiredMixin, ListView):
             region = form.cleaned_data.get("region")
             countries = form.cleaned_data.get("countries")
             archived = form.cleaned_data.get("archived")
-            queryset = self._archive_filter(queryset, archived)
 
+            if archived:
+                queryset = queryset.filter(
+                    archived__in=archived)  # pylint: disable=not-callable=)
             # Check if the statuses filter is set
             if statuses:
                 queryset = queryset.filter(status__in=statuses)
@@ -113,28 +72,22 @@ class JobListView(LoginRequiredMixin, ListView):
             if countries:
                 queryset = queryset.filter(country__in=countries)
 
-        # Return the filtered queryset
+        # Order the queryset
+        ordering = self.get_ordering()
+        if ordering:
+            queryset = queryset.order_by(ordering)
+
         return queryset
 
     def get_context_data(self, **kwargs):
-        """Get the context data for the job list view.
-
-        Adds the user ID, filter form, and a blank job form to the
-        context.
-
-        Returns:
-            dict: The context data.
-        """
+        """Get the context data for the job list view."""
         jobs = Job.objects.filter(
             profile=self.request.user.profile, archived=False).all()
         edit_forms = {job.id: JobForm(instance=job) for job in jobs}
         context = super().get_context_data(**kwargs)
         context["user_id"] = self.request.user.id
-        context["filter_form"] = JobFilterForm(
-            self.request.GET
-        )  # Pass form to template
-
+        context["filter_form"] = JobFilterForm(self.request.GET)
         context["edit_forms"] = edit_forms
-
         context["job_form"] = JobForm()
+        context["ordering"] = self.get_ordering()
         return context
